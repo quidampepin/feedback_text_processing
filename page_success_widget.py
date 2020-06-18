0632d5c5019848754d7fc4d3fd67eb51a06ee242
quidampepin <pepin.david@gmail.com>
2020-06-17 20:26:23 -0400

    Fixed classification algorithm

--- page_success_widget.py
+++ page_success_widget.py
@@ -8,7 +8,7 @@ import nltk
 from wordcloud import WordCloud
 
 #import CSV file as a Pandas dataframe
-data = pd.read_csv('page_success_may_27.csv', index_col = 0)
+data = pd.read_csv('page_success_june_17.csv', index_col = 0)
 
     #Separate English and French data
 data_en = data[data['Page URL'].str.contains("/en", na=False)]
@@ -412,18 +412,19 @@ with open('weighted_words_fr.csv', 'w') as f:
 #build training and testing sets from manually tagged data_en
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
-
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn import metrics
 
 data_en_topic_1 = data_en_topic.drop(columns=[1])
 data_en_topic_1  = data_en_topic_1.rename(columns={'Details': 'Feedback', 0: 'label'})
 y = data_en_topic_1.label
 
-X_train, X_test, y_train, y_test = train_test_split(data_en_topic_1["Feedback"], y, test_size=0.1, random_state=53)
+X_train, X_test, y_train, y_test = train_test_split(data_en_topic_1["Feedback"], y, test_size=0.23, random_state=53)
 tfidf_vectorizer = TfidfVectorizer(stop_words="english", max_df=0.7)
 tfidf_train = tfidf_vectorizer.fit_transform(X_train)
 tfidf_test = tfidf_vectorizer.transform(X_test)
 from sklearn.naive_bayes import MultinomialNB
-nb_classifier = MultinomialNB(0.6)
+nb_classifier = MultinomialNB(0.1)
 nb_classifier.fit(tfidf_train, y_train)
 pred = nb_classifier.predict(tfidf_test)
 score = metrics.accuracy_score(y_test, pred)
@@ -431,9 +432,9 @@ print(score)
 
 pred_topic_en = data_en[data_en.Topic != 'No details']
 X_pred_en = pred_topic_en['Details']
-count_pred_en = count_vectorizer.transform(X_pred_en)
-pred_en = nb_classifier.predict(count_pred_en)
-pred_topic_en["Predited_topic"] = pred_en
+tfidf_pred_en = tfidf_vectorizer.transform(X_pred_en)
+pred_en = nb_classifier.predict(tfidf_pred_en)
+pred_topic_en["Predicted_topic"] = pred_en
 pred_topic_en.to_csv('predicted_en.csv')
 
 
@@ -454,7 +455,7 @@ print(score)
 
 pred_topic_fr = data_fr[data_fr.Topic != 'No details']
 X_pred_fr = pred_topic_fr['Details']
-count_pred_fr = count_vectorizer.transform(X_pred_fr)
-pred_fr = nb_classifier.predict(count_pred_fr)
-pred_topic_fr["Predited_topic"] = pred_fr
+tfidf_pred_fr = tfidf_vectorizer.transform(X_pred_fr)
+pred_fr = nb_classifier.predict(tfidf_pred_fr)
+pred_topic_fr["Predicted_topic"] = pred_fr
 pred_topic_fr.to_csv('predicted_fr.csv')
